44157b2fc23bbd809bd4f8891d9ee57802aa0be3
junseokieo <ccobaltbluee@gmail.com>
2021-05-26 03:00:03 +0900

    12week

--- panel/src/panel/Guess.java
+++ panel/src/panel/Guess.java
@@ -2,23 +2,25 @@ package panel;
 
 import javax.swing.*;
 
-public class Guess
+public class Guess extends JFrame
 {
 	public static void main(String[] args)
 	{
 		// 프레임의 제목을 ‘홀짝 맞추기 게임’으로 한다
-      		// 여기에 코드를 삽입하세요
-
+		Guess frame = new Guess();
+		frame.setTitle("홀짝 맞추기 게임");
+		
 		// 프레임의 우측 상단에 있는 X 표시의 닫기를 클릭하면 프로그램이 종료된다
-      		// 여기에 코드를 삽입하세요
+		frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
 
 	    // 패널을 만든다
-      		// 여기에 코드를 삽입하세요
+      	GuessPanel newPanel = new GuessPanel();
 
 	    // 패널을 프레임의 컨텐트 구획에 추가한다
-      		// 여기에 코드를 삽입하세요
+      	frame.getContentPane().add(newPanel);
 
 	    // 프레임을 보여준다
-      		// 여기에 코드를 삽입하세요
+      	frame.pack();
+      	frame.setVisible(true);
 	}
 }
--- panel/src/panel/GuessPanel.java
+++ panel/src/panel/GuessPanel.java
@@ -7,87 +7,99 @@ import java.util.Random;
 
 public class GuessPanel extends JPanel
 {
-	// 객체 변수들
- private long secretNumber;	// 비밀 번호
- private boolean isEven;		// 홀짝 여부
+	private long secret;	// 비밀 번호
+	private boolean isEven;		// 홀짝 여부
 
- private JButton oddGuessButton;	// 짝수 단추
- private JButton evenGuessButton;// 홀수 단추
-
- private JLabel correctLabel;	// 정답 알림 레이블
- private JLabel wrongLabel;		// 오답 알림 레이블
-
- private JLabel answerLabel;		// 비밀 번호 알림 레이블
+	private JButton odd;	// 짝수 단추
+	private JButton even;// 홀수 단추
+	
+	private JLabel correct;	// 정답 알림 레이블
+	private JLabel wrong;		// 오답 알림 레이블	
+	private JLabel answer;		// 비밀 번호 알림 레이블
 
  // 패널을 만든다
- public GuessPanel()
- {
-		// 0부터 100 사이의 임의의 정수인 비밀 번호를 생성한다
-   		// 여기에 코드를 삽입하세요
+ public GuessPanel(){
+	 
+	 // 0부터 100 사이의 임의의 정수인 비밀 번호를 생성한다
+	 Random rand = new Random();
+	 secret = rand.nextInt(100);
 
-		// 생성된 비밀 번호가 홀수인지 아니면 짝수인지 확인한다
-   		// 여기에 코드를 삽입하세요
-
+	 // 생성된 비밀 번호가 홀수인지 아니면 짝수인지 확인한다
+	 if(secret%2 == 0) {
+		 isEven = true;
+	 }else {
+		 isEven = false;
+	 }
+	 System.out.println(secret);
+	 System.out.println(isEven);
+	 
      // 패널의 바탕색을 흰 색으로 한다
-   		// 여기에 코드를 삽입하세요
-
+	 setBackground(Color.WHITE);
+	 
      // 패널의 크기를 가로 240 픽셀, 세로 100 픽셀로 한다
-   		// 여기에 코드를 삽입하세요
-
+	 setPreferredSize(new Dimension(240, 100));
+	 
      // ‘홀수’라는 레이블을 가진 단추를 만든다
-   		// 여기에 코드를 삽입하세요
-
+	 odd = new JButton("홀수");
+	 
      // ‘짝수’라는 레이블을 가진 단추를 만든다
-   		// 여기에 코드를 삽입하세요
-
+	 even = new JButton("짝수");
 
      // 단추에서 일어나는 사건을 처리할 리스너 객체를 만든다
-   		// 여기에 코드를 삽입하세요
-
+	 ButtonListener handler = new ButtonListener();
+	 
      // 홀수 단추를 리스너에 등록한다
-   		// 여기에 코드를 삽입하세요
-
+	 odd.addActionListener(handler);
+	 
      // 짝수 단추를 리스너에 등록한다
-   		// 여기에 코드를 삽입하세요
-
+	 even.addActionListener(handler);
+	 
      // 정답 알림 레이블을 만든다
-   		// 여기에 코드를 삽입하세요
+	 correct = new JLabel("맞았습니다.");
 
      // 오답 알림 레이블을 만든다
-   		// 여기에 코드를 삽입하세요
+	 wrong = new JLabel("틀렸습니다.");
 
      // 비밀 번호 알림 레이블을 만든다
-   		// 여기에 코드를 삽입하세요
-
+	 answer = new JLabel("비밀번호 = " + secret);
+	 // 2개의 단추를 화면에 추가한다
+	 add(odd);
+	 add(even);
+	 
+	 // 3개의 레이블을 화면에 추가한다
+	 add(correct);
+	 add(wrong);
+	 add(answer);
+	 
      // 처음에 3개의 레이블들이 화면에 보여지지 않게 한다
-   		// 여기에 코드를 삽입하세요
-
-     // 2개의 단추를 화면에 추가한다
-   		// 여기에 코드를 삽입하세요
-
-
-     // 3개의 레이블을 화면에 추가한다
-   		// 여기에 코드를 삽입하세요
+	 correct.setVisible(false);
+	 wrong.setVisible(false);
+	 answer.setVisible(false);
  }
 
-	//  단추 누름 사건 리스너를 나타낸다
+ //  단추 누름 사건 리스너를 나타낸다
  private class ButtonListener implements ActionListener
  {
-		public void actionPerformed(ActionEvent event)
-		{
-			// 사건이 발생한 컴포넌트가 단추이고
-			// 정답을 맞추었으면 정답 알림 레이블을 화면에 보여지게 한다.
-			// 맞추지 못했으면 오답 알림 레이블을 화면에 보여지게 한다.
-   			// 여기에 코드를 삽입하세요
-
-         // 비밀 번호 알림 레이블을 화면에 보여지게 한다
-   			// 여기에 코드를 삽입하세요
-
-         // 정답 알림 레이블이 화면에 보여지지 않게 한다
-   			// 여기에 코드를 삽입하세요
-
-         // 오답 알림 레이블이 화면에 보여지지 않게 한다
-   			// 여기에 코드를 삽입하세요
-		}
- }
+	 public void actionPerformed(ActionEvent event)
+	 {
+		 even.setVisible(false);
+		 odd.setVisible(false);
+			
+		 if(event.getSource() == odd) { //홀수를 골랐을 때
+			 if(isEven == false) { // 홀수면
+				 correct.setVisible(true);
+				 }else {
+					 wrong.setVisible(true);
+					 }
+			 }else if(event.getSource() == even){// 짝수를 골랐을 때
+				 if(isEven == true) { // 짝수면
+					 correct.setVisible(true);
+					 }else {
+						 wrong.setVisible(true);
+						 }
+				 }
+		 // 비밀 번호 알림 레이블을 화면에 보여지게 한다
+		 answer.setVisible(true);
+		 }
+	 }
 }
